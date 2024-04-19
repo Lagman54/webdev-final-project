2458d99eb47ec659b0fc3acb361fb17fc1fd5c71
Lagman54 <aibar.kydiarxan@gmail.com>
2024-04-19 11:18:02 +0500

    frontend: add header

--- frontend/food-delivery-app/src/app/app.routes.ts
+++ frontend/food-delivery-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RestaurantsComponent } from './pages/restaurants/restaurants.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'restaurants', component: RestaurantsComponent},
+];
